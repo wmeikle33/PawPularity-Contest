a8b1e668ab75da2ccf58c87d2d1644f76055cc05
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-03-25 16:46:56 +0800

    Update config.py

--- src/pawpularity_project/config.py
+++ src/pawpularity_project/config.py
@@ -1,49 +1,10 @@
+from dataclasses import dataclass
+from pathlib import Path
 
-import pandas as pd
-import numpy as np
-import tensorflow as tf
-import matplotlib.pylab as plt
-import os
-import PIL
-from PIL import UnidentifiedImageError
-from sklearn.model_selection import StratifiedShuffleSplit
-pd.options.mode.chained_assignment = None
-
-num_rows = 162
-num_cols = 2
-data = [[None] * num_cols] * num_rows 
-newdf = pd.DataFrame(data, columns = ['Id', 'Weight'])
-
-data_path = '/Users/wmeikle/Downloads/petfinder-pawpularity-score/'
-data = pd.read_csv(data_path+'train.csv')
-
-sssplit = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
-for train_index, test_index in sssplit.split(data, data['Pawpularity']):
-    training_set = data.iloc[train_index]
-    eval_set = data.iloc[test_index]
-    
-training_set['Pawpularity'].hist(label='Training set')
-eval_set['Pawpularity'].hist(label='Eval set')
-plt.title('Pawpularity score distribution in training and test set')
-plt.xlabel('Pawpularity score')
-plt.ylabel('Count')
-plt.legend(loc='upper right')
-plt.show()
-
-
-training_set['Id'] = training_set['Id'].apply(lambda x: '/Users/wmeikle/Downloads/petfinder-pawpularity-score/train/'+x+'.jpg')
-training_set[['Id', 'Pawpularity']].to_csv('training_set.csv', header=False, index=False)
-eval_set['Id'] = eval_set['Id'].apply(lambda x: '/Users/wmeikle/Downloads/petfinder-pawpularity-score/train/'+x+'.jpg')
-eval_set[['Id', 'Pawpularity']].to_csv('eval_set.csv', header=False, index=False)
-
-IMG_WIDTH = 256
-IMG_HEIGHT = 256
-IMG_CHANNELS = 3
-
-path = '/Users/wmeikle/Downloads/petfinder-pawpularity-score/train/'
-training_img = os.listdir(path)
-rand_idx = np.random.randint(0, len(training_img)-1)
-rand_img = training_img[rand_idx]
-
-show_image(path+rand_img)
-
+@dataclass
+class Config:
+    random_state: int = 42
+    artifacts_dir: Path = Path("models")
+    model_path: Path = artifacts_dir / "model.joblib"
+    pipeline_path: Path = artifacts_dir / "pipeline.joblib"
+    vectorizer_path: Path = artifacts_dir / "vectorizer.joblib"
