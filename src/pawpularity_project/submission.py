06e7f80b79718b7bbe0c7872d9aef39eb79a84cc
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-04-15 09:39:15 +0800

    Update submission.py

--- src/pawpularity_project/submission.py
+++ src/pawpularity_project/submission.py
@@ -1,22 +1,3 @@
-Path = ''
-sample_submission = pd.read_csv('/Users/wmeikle/Downloads/petfinder-pawpularity-score/sample_submission.csv')
-sample_submission['Id'] = sample_submission['Id'].apply(lambda x: '../input/petfinder-pawpularity-score/test/'+x+'.jpg')
-sample_submission.to_csv('sample_submission.csv', index=False, header=False)
-sample_submission = tf.data.TextLineDataset(
-    'sample_submission.csv'
-).map(decode_csv).batch(BATCH_SIZE)
-
-sample_prediction = model.predict(sample_submission)
-
-submission_output = pd.concat(
-    [pd.read_csv('../input/petfinder-pawpularity-score/sample_submission.csv').drop('Pawpularity', axis=1),
-    pd.DataFrame(sample_prediction)],
-    axis=1
-)
-submission_output.columns = [['Id', 'Pawpularity']]
-
-submission_output.to_csv('submission.csv', index=False)
-
 def parse_args():
     ap = argparse.ArgumentParser()
     ap.add_argument("--model", default="models/model.joblib")
