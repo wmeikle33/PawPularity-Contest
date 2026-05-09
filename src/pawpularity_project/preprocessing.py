96a796f794e56188749b86621e2efb20df321ba9
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-09 11:52:29 +0800

    Update preprocessing.py

--- src/pawpularity_project/preprocessing.py
+++ src/pawpularity_project/preprocessing.py
@@ -1,3 +1,25 @@
+def read_and_decode(filename, reshape_dims):
+    # Read an image file to a tensor as a sequence of bytes
+    image = tf.io.read_file(filename)
+    # Convert the tensor to a 3D uint8 tensor
+    image = tf.image.decode_jpeg(image, channels=IMG_CHANNELS)
+    # Convert 3D uint8 tensor with values in [0, 1]
+    image = tf.image.convert_image_dtype(image, tf.float32)
+    # Resize the image to the desired size
+    return tf.image.resize(image, reshape_dims)
+
+def show_image(filename):
+    image = read_and_decode(filename, [IMG_HEIGHT, IMG_WIDTH])
+    plt.imshow(image.numpy());
+    plt.axis('off');
+    
+def decode_csv(csv_row):
+    record_defaults = ['Id', 'Weight']
+    filename, pawpularity = tf.io.decode_csv(csv_row, record_defaults)
+    pawpularity = tf.convert_to_tensor(float(pawpularity), dtype=tf.float32)
+    image = read_and_decode(filename, [IMG_HEIGHT, IMG_WIDTH])
+    return image, pawpularity
+    
 def stratified_split(data, n_splits, test_size):
     sssplit = StratifiedShuffleSplit(n_splits=1, test_size=0.2)
     for train_index, test_index in sssplit.split(data, data['Pawpularity']):
