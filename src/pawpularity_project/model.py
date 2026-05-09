c8038d1ce7ee9278e86863b6adf713a18fd62bea
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-09 18:47:27 +0800

    Update model.py

--- src/pawpularity_project/model.py
+++ src/pawpularity_project/model.py
@@ -15,13 +15,6 @@ IMG_HEIGHT = 128
 IMG_WIDTH = 128
 IMG_CHANNELS = 3
 
-
-train_dataset = tf.data.TextLineDataset(
-    'training_set.csv').map(decode_csv).batch(BATCH_SIZE)
-
-eval_dataset = tf.data.TextLineDataset(
-    'eval_set.csv').map(decode_csv).batch(BATCH_SIZE)
-
 def build_model(
     img_height: int = IMG_HEIGHT,
     img_width: int = IMG_WIDTH,
