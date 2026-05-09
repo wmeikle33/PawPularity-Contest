e28dfdff0e4c6e3fe7599e6d955fdad7fffba591
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-09 15:07:26 +0800

    Update conftest.py

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import pytest
 
 @pytest.fixture
 def sample_ctr_df():
-    """Small synthetic dataset for testing."""
     images = np.random.rand(100, 28, 28).astype(np.float32)
     labels = np.random.randint(0, 10, size=(100,))
     dataset = tf.data.Dataset.from_tensor_slices((images, labels))
