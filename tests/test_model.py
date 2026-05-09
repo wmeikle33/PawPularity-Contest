b5aada04e51bf97494d88b954a0023b2bb748166
William Meikle <80005114+wmeikle33@users.noreply.github.com>
2026-05-09 12:14:47 +0800

    Update test_model.py

--- tests/test_model.py
+++ tests/test_model.py
@@ -4,6 +4,10 @@ from pawpularity_project.model import build_pipeline, train_eval_save, load_mode
 
 
 def make_small_training_df():
+  images = np.random.rand(100, 28, 28).astype(np.float32) 
+  labels = np.random.randint(0, 10, size=(100,))
+  dataset = tf.data.Dataset.from_tensor_slices((images, labels))
+  dataset = dataset.batch(32)
 
 def test_build_pipeline_can_fit_and_predict():
 
